79a4965a63dd4c6256f93c9ceae67703ca8541c3
zhangchengpeng <314899581@qq.com>
2018-03-08 20:46:27 +0800

    增加提交脚本

---
+++ commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+:<<!
+LOG_FILE="/var/log/commit.log"
+>"${LOG_FILE}"
+exec &>>${LOG_FILE}
+set -x
+!
+
+set -x
+
+main()
+{
+	git pull
+	git add .
+	local note=$1
+	git commit -m "${note}" 
+	git push -u origin master
+}
+
+main "$@"
